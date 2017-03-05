c8f80d00446cde8ff8a2a5c63a66899b08e975a0
Inanc Gumus <inanc@pixenka.com>
2017-03-06 00:01:41 +0300

    update: tab bar coloring

--- chat.screen.js
+++ chat.screen.js
@@ -7,4 +7,18 @@ import ChatHistoryScreen from './chat.history.screen.js'
 export const ChatScreenNavigator = TabNavigator({
   Main: { screen: ChatMainScreen },
   History: { screen: ChatHistoryScreen },
+}, {
+  tabBarOptions: {
+    labelStyle: {
+      fontSize: 14,
+      color: 'black',
+    },
+    style: {
+      height: 40,
+      padding: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'gray'
+    }
+  }
 })
